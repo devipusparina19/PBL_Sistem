30cdeb92cc53c592d264569a1ce64ce474dfdba0
devipusparina19 <devi.pusparina@mhs.politala.ac.id>
2025-09-25 15:51:47 +0700

    milestone

--- app/Models/Milestone.php
+++ app/Models/Milestone.php
@@ -1,9 +1,14 @@
+<?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Milestone extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'tanggal',
         'judul',
