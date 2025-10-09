586a81db69be3b4889798e0b9483d9ec5acebf82
ciaaaa22 <azkia.zain@mhs.politala.ac.id>
2025-10-09 10:35:38 +0800

    nah

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\HomeController;
@@ -27,6 +25,7 @@ use App\Http\Controllers\LoginController;
 | AUTH & LOGIN / REGISTER
 |--------------------------------------------------------------------------
 */
+
 // ================================
 // HOME / DASHBOARD
 // ================================
@@ -64,38 +63,17 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact.stor
 Route::middleware('auth')->group(function () {
 
     // ======= Dashboard Per Role =======
-    // Dashboard Mahasiswa
-Route::get('/dashboard/mahasiswa', function () {
-    return view('dashboard.mahasiswa');
-})->name('mahasiswa.dashboard')->middleware('auth');
-
-// Dashboard Dosen
-Route::get('/dashboard/dosen', function () {
-    return view('dashboard.dosen');
-})->name('dosen.dashboard')->middleware('auth');
-
-// Dashboard Admin
-Route::get('/dashboard/admin', function () {
-    return view('dashboard.admin');
-})->name('admin.dashboard')->middleware('auth');
-
-// Dashboard Koordinator PBL
-Route::get('/dashboard/koordinator_pbl', function () {
-    return view('dashboard.koordinator_pbl');
-})->name('koordinator_pbl.dashboard')->middleware('auth');
-
-// Dashboard Koordinator Prodi
-Route::get('/dashboard/koordinator_prodi', function () {
-    return view('dashboard.koordinator_prodi');
-})->name('koordinator_prodi.dashboard')->middleware('auth');
+    Route::get('/dashboard/mahasiswa', fn() => view('dashboard.mahasiswa'))->name('mahasiswa.dashboard');
+    Route::get('/dashboard/dosen', fn() => view('dashboard.dosen'))->name('dosen.dashboard');
+    Route::get('/dashboard/admin', fn() => view('dashboard.admin'))->name('admin.dashboard');
+    Route::get('/dashboard/koordinator_pbl', fn() => view('dashboard.koordinator_pbl'))->name('koordinator_pbl.dashboard');
+    Route::get('/dashboard/koordinator_prodi', fn() => view('dashboard.koordinator_prodi'))->name('koordinator_prodi.dashboard');
 
     // ======= CRUD Data =======
     Route::resource('mahasiswa', MahasiswaController::class);
     Route::resource('dosen', DosenController::class);
     Route::resource('kelompok', KelompokController::class);
     Route::resource('logbook', LogbookController::class);
-
-    
 
     /*
     |--------------------------------------------------------------------------
@@ -152,3 +130,58 @@ Route::get('/dashboard/koordinator_prodi', function () {
     // Dashboard Kelompok
     Route::get('/dashboard/kelompok', fn() => view('dashboard.kelompok'))->name('dashboard.kelompok');
 });
+
+/*
+|--------------------------------------------------------------------------
+| ROUTE LAINNYA (AUTENTIKASI)
+|--------------------------------------------------------------------------
+*/
+
+// Mata Kuliah
+Route::resource('mata_kuliah', MataKuliahController::class)->middleware('auth');
+
+// User
+Route::resource('user', UserController::class)->middleware('auth');
+Route::get('/user', [UserController::class, 'index'])->name('user.index')->middleware('auth');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create')->middleware('auth');
+Route::post('/user', [UserController::class, 'store'])->name('user.store')->middleware('auth');
+Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show')->middleware('auth');
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit')->middleware('auth');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update')->middleware('auth');
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy')->middleware('auth');
+
+// Dosen
+Route::get('/dosen', [DosenController::class, 'index'])->name('dosen.index')->middleware('auth');
+Route::get('/dosen/create', [DosenController::class, 'create'])->name('dosen.create')->middleware('auth');
+Route::post('/dosen', [DosenController::class, 'store'])->name('dosen.store')->middleware('auth');
+Route::get('/dosen/{id}', [DosenController::class, 'show'])->name('dosen.show')->middleware('auth');
+Route::get('/dosen/{id}/edit', [DosenController::class, 'edit'])->name('dosen.edit')->middleware('auth');
+Route::put('/dosen/{id}', [DosenController::class, 'update'])->name('dosen.update')->middleware('auth');
+Route::delete('/dosen/{id}', [DosenController::class, 'destroy'])->name('dosen.destroy')->middleware('auth');
+
+// Mahasiswa
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index')->middleware('auth');
+Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create')->middleware('auth');
+Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.store')->middleware('auth');
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show'])->name('mahasiswa.show')->middleware('auth');
+Route::get('/mahasiswa/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit')->middleware('auth');
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update')->middleware('auth');
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy')->middleware('auth');
+
+// Kelompok
+Route::get('/kelompok', [KelompokController::class, 'index'])->name('kelompok.index')->middleware('auth');
+Route::get('/kelompok/create', [KelompokController::class, 'create'])->name('kelompok.create')->middleware('auth');
+Route::post('/kelompok', [KelompokController::class, 'store'])->name('kelompok.store')->middleware('auth');
+Route::get('/kelompok/{id}', [KelompokController::class, 'show'])->name('kelompok.show')->middleware('auth');
+Route::get('/kelompok/{id}/edit', [KelompokController::class, 'edit'])->name('kelompok.edit')->middleware('auth');
+Route::put('/kelompok/{id}', [KelompokController::class, 'update'])->name('kelompok.update')->middleware('auth');
+Route::delete('/kelompok/{id}', [KelompokController::class, 'destroy'])->name('kelompok.destroy')->middleware('auth');
+
+// Logbook
+Route::get('/logbook', [LogbookController::class, 'index'])->name('logbook.index')->middleware('auth');
+Route::get('/logbook/create', [LogbookController::class, 'create'])->name('logbook.create')->middleware('auth');
+Route::post('/logbook', [LogbookController::class, 'store'])->name('logbook.store')->middleware('auth');
+Route::get('/logbook/{id}', [LogbookController::class, 'show'])->name('logbook.show')->middleware('auth');
+Route::get('/logbook/{id}/edit', [LogbookController::class, 'edit'])->name('logbook.edit')->middleware('auth');
+Route::put('/logbook/{id}', [LogbookController::class, 'update'])->name('logbook.update')->middleware('auth');
+Route::delete('/logbook/{id}', [LogbookController::class, 'destroy'])->name('logbook.destroy')->middleware('auth');
