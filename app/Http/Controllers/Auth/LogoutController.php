9d0198cc4d5d4bcdce1ff46bf0ef467263dc0337
devipusparina19 <devi.pusparina@mhs.politala.ac.id>
2025-10-14 10:19:55 +0700

    Integrasi Akun Google Done

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -8,13 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
+    /**
+     * Proses logout pengguna dari sistem (manual maupun Google).
+     */
     public function logout(Request $request)
     {
+        // Logout user
         Auth::logout();
 
+        // Hapus semua session
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login')->with('success', 'Anda telah berhasil logout.');
+        // Arahkan ke halaman login dengan notifikasi sukses
+        return redirect()->route('login')->with('success', 'Anda telah berhasil logout.');
     }
 }
